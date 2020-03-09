d87692cbdfd950b352b8d3fdd6fff267e81b7baf
Piotr Lasota <lasota.piotr1@gmail.com>
2020-03-09 23:30:40 +0100

    fix: downloading source.zip

--- backend/src/utils/generateSourcePackage.ts
+++ backend/src/utils/generateSourcePackage.ts
@@ -1,25 +1,24 @@
 import AdmZip from 'adm-zip'
-import util from 'util'
-
-const exec = util.promisify(require('child_process').exec)
+import fs from 'fs'
+import path from 'path'
 
 export const generateSourcePackage = async () => {
   const zip = new AdmZip()
-  const { stdout, stderr }: { stdout: string; stderr: any } = await exec(
-    'git ls-files --exclude-standard'
-  )
-  if (stderr) {
-    console.error(`error: ${stderr}`)
-  }
-  const files = stdout.trim().split('\n')
-  const paths = files.map(file => file.split('/').slice(0, -1).join('/') || undefined)
-  console.log({
-    files,
-    files1: files.map(file => file.split('/').slice(0, -1).join('/') || undefined)
-  })
 
-  files.forEach((file, index) => {
-    zip.addLocalFile(file, paths[index])
+  const directoryPath = path.join(__dirname, '../..')
+
+  const files: string[] = fs.readdirSync(directoryPath)
+  files.forEach(function(file) {
+    if (file === 'node_modules' || file === '.env') {
+      return
+    }
+    const filePath = path.join(directoryPath, file)
+    const stat = fs.statSync(filePath)
+    if (stat.isDirectory()) {
+      zip.addLocalFolder(file)
+    } else {
+      zip.addLocalFile(file)
+    }
   })
   zip.writeZip('./dist/source.zip')
 }
